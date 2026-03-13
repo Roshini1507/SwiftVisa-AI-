afbfafd40f6e8b4eb315db106f39c0c70e6679ad
Roshini <broshini909@gmail.com>
2026-03-13 23:29:52 +0530

    Support Streamlit secrets for API key

--- config/config.py
+++ config/config.py
@@ -1,9 +1,13 @@
 import os
+import streamlit as st
 from dotenv import load_dotenv
 
 load_dotenv()
 
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+
+if not GOOGLE_API_KEY:
+    GOOGLE_API_KEY = st.secrets["GOOGLE_API_KEY"]
 
 VECTORSTORE_PATH = "vectorstore"
 DATA_PATH = "data"
